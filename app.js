5618a3ec18fa2b38f3c24843111c7182990d3faf
archit-rohal <rohalarchit@gmail.com>
2023-01-08 13:42:01 +0530

    take input and console

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
 function clickHandeler(){
     console.log('Clicked!');
+    console.log('Input', txtInput.value);
 };
 btnTranslate.addEventListener("click", clickHandeler)

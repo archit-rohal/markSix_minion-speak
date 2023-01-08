dfda7ba0eed5f572581157789c9088d9ba36d5ed
archit-rohal <rohalarchit@gmail.com>
2023-01-08 23:18:25 +0530

    markSix: Tanay-like

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Sorry! Something is wrong with the server. Try again after some time")
+}
 function clickHandeler(){
-    outputDiv.innerText = "blahblahblehbleh " + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText)) //Here getTranslation is first called and the program - flow has shifted upwards to the function
+        .then(json => {                 
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
 };
 btnTranslate.addEventListener("click", clickHandeler)
